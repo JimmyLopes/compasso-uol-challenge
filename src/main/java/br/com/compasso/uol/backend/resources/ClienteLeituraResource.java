a2e8163fe64d8e9ccf8b4b4775fb8fade5aec0f1
Jimmy Botezine <jbotezine@gmail.com>
2020-12-16 22:59:10 -0300

    feat: Criado endpoints para buscar cliente por nome ou por ID

---
+++ src/main/java/br/com/compasso/uol/backend/resources/ClienteLeituraResource.java
@@ -0,0 +1,35 @@
+package br.com.compasso.uol.backend.resources;
+
+import br.com.compasso.uol.backend.dtos.ClienteRetornadoDto;
+import br.com.compasso.uol.backend.models.Cliente;
+import br.com.compasso.uol.backend.services.ClienteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/compasso-uol/cliente")
+public class ClienteLeituraResource {
+
+    private final ClienteService clienteService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ClienteLeituraResource(ClienteService clienteService, ModelMapper modelMapper) {
+        this.clienteService = clienteService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/nome-procurado")
+    public ResponseEntity<ClienteRetornadoDto> buscarClientePeloNome(@RequestParam String nomeCompleto){
+        Cliente cliente = clienteService.buscarClientePorNome(nomeCompleto);
+        return ResponseEntity.ok().body(modelMapper.map(cliente, ClienteRetornadoDto.class));
+    }
+
+    @GetMapping("/{id}/procurado")
+    public ResponseEntity<ClienteRetornadoDto> buscarClientePeloId(@PathVariable(value = "id") Long idCliente){
+        Cliente cliente = clienteService.buscarPorId(idCliente);
+        return ResponseEntity.ok().body(modelMapper.map(cliente, ClienteRetornadoDto.class));
+    }
+}
